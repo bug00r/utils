70a72ef55b087f40cbb932f2b91ec677e8fba7cf
bug00r <bachus@gmx.net>
2022-07-07 11:09:15 +0200

    structuring

--- src/byte_utils.c
+++ src/byte_utils.c
@@ -1,5 +1,40 @@
 #include "byte_utils.h"
 
+static void __byte_buffer_append_bytes_trunc(byte_buffer_t* _buffer, unsigned char* bytes, size_t cntBytes)
+{
+	byte_buffer_t* buffer = _buffer;
+	size_t curOffset = buffer->offset;
+	int untilEndBytes = buffer->size - curOffset;
+
+	//not space left and TRUNCMODE stopps here
+	if ( untilEndBytes == 0 ) return;
+
+	size_t cntCopyBytes = ( untilEndBytes < cntBytes ? untilEndBytes : cntBytes);
+
+	memcpy( buffer->buffer + curOffset, bytes, cntCopyBytes );
+
+	buffer->offset += cntCopyBytes;
+}
+
+static void __byte_buffer_append_bytes_skip(byte_buffer_t* _buffer, unsigned char* bytes, size_t cntBytes)
+{
+	byte_buffer_t* buffer = _buffer;
+	if ( (buffer->offset + cntBytes) >= buffer->size ) return;
+
+	__byte_buffer_append_bytes_trunc(buffer, bytes, cntBytes);
+}
+
+static void __byte_buffer_append_bytes_ring(byte_buffer_t* _buffer, unsigned char* bytes, size_t cntBytes)
+{
+	byte_buffer_t* buffer = _buffer;
+
+	for ( size_t curByte = 0; curByte < cntBytes; curByte++ )
+	{
+		byte_buffer_append_byte(buffer, bytes[curByte]);
+	}
+}
+
+
 //Took outside buffer to work on it
 void byte_buffer_init(byte_buffer_t* _buffer, 
                       byte_buffer_mode_t mode, 
@@ -142,40 +177,6 @@ void byte_buffer_append_byte(byte_buffer_t* _buffer, unsigned char byte)
 	}
 }
 
-static void __byte_buffer_append_bytes_trunc(byte_buffer_t* _buffer, unsigned char* bytes, size_t cntBytes)
-{
-	byte_buffer_t* buffer = _buffer;
-	size_t curOffset = buffer->offset;
-	int untilEndBytes = buffer->size - curOffset;
-
-	//not space left and TRUNCMODE stopps here
-	if ( untilEndBytes == 0 ) return;
-
-	size_t cntCopyBytes = ( untilEndBytes < cntBytes ? untilEndBytes : cntBytes);
-
-	memcpy( buffer->buffer + curOffset, bytes, cntCopyBytes );
-
-	buffer->offset += cntCopyBytes;
-}
-
-static void __byte_buffer_append_bytes_skip(byte_buffer_t* _buffer, unsigned char* bytes, size_t cntBytes)
-{
-	byte_buffer_t* buffer = _buffer;
-	if ( (buffer->offset + cntBytes) >= buffer->size ) return;
-
-	__byte_buffer_append_bytes_trunc(buffer, bytes, cntBytes);
-}
-
-static void __byte_buffer_append_bytes_ring(byte_buffer_t* _buffer, unsigned char* bytes, size_t cntBytes)
-{
-	byte_buffer_t* buffer = _buffer;
-
-	for ( size_t curByte = 0; curByte < cntBytes; curByte++ )
-	{
-		byte_buffer_append_byte(buffer, bytes[curByte]);
-	}
-}
-
 void byte_buffer_append_bytes(byte_buffer_t* _buffer, unsigned char* bytes, size_t cntBytes)
 {
 	byte_buffer_t* buffer = _buffer;
@@ -196,42 +197,7 @@ void byte_buffer_append_bytes(byte_buffer_t* _buffer, unsigned char* bytes, size
 				__byte_buffer_append_bytes_trunc(buffer, bytes, cntBytes);
 				break;
 		}
-		/*
-		size_t curOffset = buffer->offset;
-		int untilEndBytes = buffer->size - curOffset;
-		int restBytes = cntBytes - untilEndBytes;
 
-		if ( restBytes < 0 ) restBytes = 0;
-
-		bool isOverflow = (untilEndBytes == 0) || (restBytes > 0) ;
-		
-		if ( isOverflow && buffer->mode == BYTE_BUFFER_SKIP ) return;
-
-		for
-		*/
-
-		/*size_t usedOffset = buffer->offset;
-		bool isOverflow = (usedOffset >= buffer->size);
-
-		if (isOverflow)
-		{
-			switch(buffer->mode)
-			{
-				case BYTE_BUFFER_TRUNCATE:
-				case BYTE_BUFFER_SKIP:
-										return;
-				case BYTE_BUFFER_RING:  usedOffset = 0;
-										buffer->offset = usedOffset + 1;
-										break;
-				default: return;
-			}
-		}
-		else {
-			buffer->offset++;
-		}
-			
-		buffer->buffer[usedOffset] = byte;
-		*/
 	}
 }
 
